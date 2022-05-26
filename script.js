35051a55d8610fc27292d53959f3cb078e33457b
Laudi <aleckoles@gmail.com>
2022-05-27 00:00:35 +0300

    5 tasks

--- script.js
+++ script.js
@@ -12,3 +12,29 @@ str = str.split(',');
 console.log(str);
 str = str.join(';');
 console.log(str);
+
+const numFirst = [32, 31, 34, 36, 37, 43, 57, 89, 30, 49];
+const [numOne, numTwo, ...numSecond] = numFirst;
+console.log(numOne, numTwo);
+console.log(numSecond);
+
+const numFirst = [32, 31, 34, 36, 37, 43, 57, 89, 30, 49];
+console.log(numFirst);
+let maxM = Math.max(...numFirst);
+console.log(maxM + ' is maximum');
+
+const numFirst = [32, 31, 34, 36, 37];
+console.log('numFirst is ' + numFirst);
+const numSecond = [...numFirst];
+/* for (let i = 0; i < numSecond.length; i++) {
+  numSecond[i] = numSecond[i]*2;
+}
+*/
+for (i in numSecond) {
+    numSecond[i] = numSecond[i] * 2;
+}
+
+/* numSecond.foreach ()
+ */
+console.log(`numSecond is ${numSecond}`);
+console.log('numFirst is ' + numFirst);
